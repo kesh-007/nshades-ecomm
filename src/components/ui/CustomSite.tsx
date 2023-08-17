6c35a37831317a296976a9c3fb90cc1c6a6ac713
harishm <harishm762003@gmail.com>
2023-08-17 11:01:48 +0530

    Added Image Alignment option and Company name to template page.

--- src/components/ui/CustomSite.tsx
+++ src/components/ui/CustomSite.tsx
@@ -1,30 +1,42 @@
-import { useState } from "react";
+export default function TemplatePage({
+    heading , image_align, company_name,
+     bgimage , description , image ,
+      about_description , about_description_title ,
 
-export default function TemplatePage({heading ,
-     bgimage , description , image , about_description , about_description_title }){
+    }){
 
     return (
     <div className="h-full relative text-4xl overflow-y-auto ">
-
+        <div className="text-gray-400 absolute text-white">
+            <p>Home</p>
+        </div>
         <div className="relative h-full overflow-hidden">
             <img
             src={bgimage}
             className=" object-fit h-full w-full  object-cover"/>
-            <div className="absolute inset-0 bg-black opacity-[0.5]"></div>
-        </div>
-        <div className=" w-full flex items-center flex-col gap-10 centered-text absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center  text-white">
-            <p className=" text-9xl">{heading}</p>
-            <div className="w-[70%]  p-5 whitespace-normal">
-                <p className="text-xl text-gray-400 text-center">{description}</p>
+            <div className="absolute inset-0 bg-black opacity-[0.5] p-5">
+                <p className="text-white">{company_name}</p>
             </div>
         </div>
-        <div className="p-10 flex h-screen items-center bg-white">
+        <div className=" w-full flex items-center flex-col gap-10 centered-text absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center  text-white">
+            <p className=" text-6xl">{heading}</p>
+            <div className="w-[70%]  p-5 ">
+                <p className="text-xl text-gray-400  whitespace-normal break-words">{description}</p>
+            </div>
+        </div>
+        <div className={`p-10 flex h-screen items-center bg-white ${(!image_align)?"flex-row-reverse":"flex-row"}`} >
             <div className="w-full pl-10">
-                <img className=" rounded shadow-lg shadow-gray-500/40 h-2/3 object-contain" src={image}/>
+                <img className="rounded shadow-lg shadow-gray-500/40 h-2/3 object-contain" src={image}/>
             </div>
             <div className="w-full p-3 h-screen flex item-center flex-col justify-center gap-11 m-10">  
                 <p className="text-4xl">{about_description_title}</p>
                 <p className="text-gray-500 text-2xl">{about_description}</p>
+            </div>
+        </div>
+        <div>
+            <p></p>
+            <div>
+                {}
             </div>
         </div>
 </div>
--- src/components/ui/Template.tsx
+++ src/components/ui/Template.tsx
@@ -1,7 +1,18 @@
 import { useState } from "react";
 import TemplatePage from "./CustomSite";
 import { GiHamburgerMenu } from "react-icons/gi";
-import Link from "next/link";
+//import Link from "next/link";
+
+
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+  } from "~/components/ui/select"
 
 
 export default function  Template( {trigger , nav}){
@@ -14,6 +25,9 @@ export default function  Template( {trigger , nav}){
     const [pageAboutDescription , setPageAboutDescription] = useState("Wedrops: Elevating Eye and Ear Care. Discover a range of meticulously crafted drops designed to enhance your eye and ear health. Our premium solutions combine science and care to provide soothing relief and lasting well-being.");
     const [pageImage , setPageImage] = useState("https://d2hg8ctx8thzji.cloudfront.net/healthpanda.net/wp-content/uploads/2020/02/5_effective_treatment_options_for_dry_eyes-715x505.jpg");
     const [pageBgImage , setPageBgImage]  = useState("https://images.pexels.com/photos/531880/pexels-photo-531880.jpeg");
+    const [pageImageAlignLeft , setPageImageAlignLeft] = useState(true);
+    const [pageCompanyName , setPageCompanyName] = useState("YourCompanyName");
+
     return (
         <div className="h-screen w-screen">
             <div className="w-full flex justify-between p-3 items-center">
@@ -38,9 +52,17 @@ export default function  Template( {trigger , nav}){
                     {/* left section content */}
                     <div className="p-3 flex flex-col gap-10 overflow-y-auto  h-full">
                         <div className="flex flex-col gap-2">
-                            <label>Heading</label>
+                            <label>Company Name</label>
                             <input 
                             maxLength={15}
+                            onChange={(e) => setPageCompanyName(e.target.value)}
+                            className="border p-2 rounded m-1" 
+                            placeholder="Default Home Page"/>
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <label>Heading</label>
+                            <input 
+                            maxLength={35}
                             onChange={(e) => setPageHeading(e.target.value)}
                             className="border p-2 rounded m-1" 
                             placeholder="Default Home Page"/>
@@ -48,15 +70,31 @@ export default function  Template( {trigger , nav}){
                         <div className="flex flex-col gap-2">
                             <label>Text</label>
                             <textarea 
-                            cols={100}
+                            cols={80}
+                            maxLength={80}
                             onChange={(e) => setPageDescription(e.target.value)}
-                            className = "border resize-none outline-none p-2 h-[10rem]" placeholder="Default Home Page"/>
+                            className = "border resize-none outline-none p-2 h-[10rem]" placeholder={pageDescription}/>
                         </div>
-                        <div className="flex flex-col gap-2">
-                            <label>Image Alignment</label>
-                            <input 
+                        <div className="flex flex-col gap-2 w-full">
+                            <label>Align Image</label>
+                            {/* <select
                             className="border p-2 rounded m-1" 
-                            placeholder="Default Home Page"/>
+                            placeholder="Default Home Page" onChange={(e) => setPageImageAlignLeft(!pageImageAlignLeft)}>
+                                <option>Left</option>
+                                <option>Right</option>
+                            </select> */}
+                            <Select onValueChange={(e) => setPageImageAlignLeft((e === "left")? true : false) }>
+                                <SelectTrigger className="outline-none">
+                                    <SelectValue placeholder="Alignment" />
+                                </SelectTrigger>
+                                <SelectContent className="w-full">
+                                    <SelectGroup >
+                                        <SelectLabel>Alignment</SelectLabel>
+                                        <SelectItem value="left" >Left</SelectItem>
+                                        <SelectItem value="right">Right</SelectItem>
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
                         </div>
                         <div className="flex flex-col gap-2">
                             <label>Layout</label>
@@ -92,7 +130,9 @@ export default function  Template( {trigger , nav}){
                         description={pageDescription}
                         image = {pageImage}
                         about_description={pageAboutDescription}
+                        image_align = {pageImageAlignLeft}
                         about_description_title={pageAboutDescriptionTitle}
+                        company_name={pageCompanyName}
                         /> {/*GENERATED TEMPLATE PAGE*/}
                     </div>
                 </div>
